d6bc701767567a461750828394137ce8a4e475ee
dev-xero <elo48858@gmail.com>
2024-04-09 14:11:16 +0100

    feat: Implement -w flag

--- wc.c
+++ wc.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+#define IN 1
+#define OUT 0
 
 int getByteCount(FILE *fptr)
 {
@@ -28,6 +32,30 @@ int getLineCount(FILE *fptr)
     return count;
 }
 
+int getWordCount(FILE *fptr)
+{
+    int count = 0;
+    char c;
+
+    int STATE = OUT;
+
+    while ((c = fgetc(fptr)) != EOF)
+    {
+        if (isspace(c))
+            STATE = OUT;
+
+        else if (STATE == OUT)
+        {
+            ++count;
+            STATE = IN;
+        }
+    }
+
+    rewind(fptr);
+
+    return count;
+}
+
 int main(int argc, char **argv)
 {
     if (argc > 2)
@@ -41,16 +69,25 @@ int main(int argc, char **argv)
         if (fptr == NULL)
             return 1;
 
+        // -c flag counts bytes
         if (strcmp(flag, "-c") == 0)
         {
             int bytes = getByteCount(fptr);
             printf("\t%d %s", bytes, filePath);
         }
 
+        // -l flag counts lines
         if (strcmp(flag, "-l") == 0)
         {
             int lines = getLineCount(fptr);
             printf("\t%d %s", lines, filePath);
+        }
+
+        // -w flag counts words
+        if (strcmp(flag, "-w") == 0)
+        {
+            int words = getWordCount(fptr);
+            printf("\t%d %s", words, filePath);
         }
     }
 
